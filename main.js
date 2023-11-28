04eb41b43a5983bbbac74da0d61afa4cf474e6bc
Shandon Vasquez <shandonvasquez1@gmail.com>
2023-11-27 22:24:51 -0600

    This has some changes to the main,js

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-var button = document.getElementById('button');
-var input = document.getElementById('input');
-var ul = document.querySelector('ul');
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-button.addEventListener('click', function() {
-    if (input.value.length > 0) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = '';
-    }
-    });
+function inputLength() {
+    return input.value.length;
+}
 
-input.addEventListener('keypress', function(event) {
-    if (input.value.length > 0 && event.keyCode === 13) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = '';
+function createListElement() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = ""; // Corregido el signo "=" en lugar de "-"
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
     }
-    });
-    
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
